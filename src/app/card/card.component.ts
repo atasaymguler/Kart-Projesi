e877c37379fd98ca30f9297315063773e3bb5ca4
Atasay <130931984+atasaymguler@users.noreply.github.com>
2023-08-27 15:13:21 +0300

    Html Çoklama ve Component Oluşturma

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card', //bu selectoru kullanarak başka bir yerden bunu çağırabilirsin, istenilen her yerden birden çok kez ulaşılabilir
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+
+}
