999ee6657ed59d6f8c961d41787a2d40b50a1ac9
Atasay <130931984+atasaymguler@users.noreply.github.com>
2023-08-28 14:03:45 +0300

    Bir Componentten Diğerine Data Geçme

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card', //bu selectoru kullanarak başka bir yerden bunu çağırabilirsin, istenilen her yerden birden çok kez ulaşılabilir
@@ -7,25 +7,10 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
 
-  posts = 
-  [
-    {
-     title:"Dağ Bisikleti",
-     imageUrl:"assets/biking.jpeg", 
-     username:"cbdag", 
-     content:"Çok hızlı sürdüm"
-    },
-    {
-      title:"Tırmanış",
-      imageUrl:"assets/mountain.jpeg", 
-      username:"cbuludag", 
-      content:"Bugün iyi tırmandım"
-    }, {
-      title:"Doğa Yürüyüşü",
-      imageUrl:"assets/tree.jpeg", 
-      username:"cbdoga", 
-      content:"Hadi biraz Maçka Parkında turluyalım"
-     }
-  ]
+  @Input() baslik:string = "";
+  @Input() resimYolu:string = "";
+  @Input() kullaniciAdi:string = "";
+  @Input() yorum:string = "";
+  
 
 }
